117f7ba6f8ba0153d972218b28a4f0f9cf13c012
Bruno M Musamali <94826679+bruno-musamali@users.noreply.github.com>
2026-01-22 16:04:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Updated products with local images
+const products = [
+    { 
+        id: 1, 
+        name: 'Pink Signature T-Shirt', 
+        price: 20, 
+        image: 'images/tshirt.jpg' 
+    },
+    { 
+        id: 2, 
+        name: 'Slim Fit Jeans', 
+        price: 50, 
+        image: 'images/jeans.jpg' 
+    },
+    { 
+        id: 3, 
+        name: 'Classic Sneakers', 
+        price: 80, 
+        image: 'images/sneakers.jpg' 
+    }
+];
